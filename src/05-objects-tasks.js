6609a5c347cc088b6eb2c6f39346d502495cf999
pavelVolkau <lekoo.cwalk@gmail.com>
2022-07-27 00:50:58 +0300

    56 pas

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -19,8 +19,14 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(_width, _height) {
+  return {
+    width: _width,
+    height: _height,
+    getArea: function getArea() {
+      return this.width * this.height;
+    },
+  };
 }
 
 /**
@@ -33,8 +39,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
